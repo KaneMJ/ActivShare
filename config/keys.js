a70ffe11976a306bd48abdf3163c990a1f3d74a0
KaneMJ <matthewjkane@live.com>
2018-05-03 14:30:46 -0700

    Added keys file and put the real credentials on gitignore

---
+++ config/keys.js
@@ -0,0 +1,5 @@
+module.exports = {
+    mongoURI: 'mongodb://dev:devTime2020@ds113358.mlab.com:13358/activshare',
+    googleClientID: '24557039894-e1dnls5i97rchccclg7qgb9hl5jc45vq.apps.googleusercontent.com',
+    googleClientSecret: 'zjnfxSSmHgDWxnXSGjH1__J8'
+}
