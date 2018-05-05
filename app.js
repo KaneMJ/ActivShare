b6b86f4e34fe7a588da9d3de735bd6d769ee6057
KaneMJ <matthewjkane@live.com>
2018-05-05 10:59:04 -0700

    Added verify and logout page, also added global vars

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cookieParser = require('cookie-parser');
+const session = require('express-session');
 const passport = require('passport');
 
 const app = express();
@@ -28,12 +30,25 @@ app.get('/', (req, res) => {
     res.send('Hello');
 });
 
-// Use Routes
-app.use('/auth', auth);
+app.use(cookieParser());
+app.use(session({
+    secret: 'secret',
+    resave: false,
+    saveUninitialized: false
+}));
 
-// Passport iddleware
+// Passport Middleware
 app.use(passport.initialize());
 app.use(passport.session());
+
+// Set Global Vars
+app.use((req, res, next) => {
+    res.locals.user = req.user || null;
+    next();
+});
+
+// Use Routes
+app.use('/auth', auth);
 
 app.listen(port, () => {
     console.log(`Server started on port ${port}`)
