f0d89d3bcc6c983176db707b66e70354c8523064
KaneMJ <matthewjkane@live.com>
2018-05-04 19:14:20 -0700

    took a pause after doing google passports check

--- app.js
+++ app.js
@@ -1,12 +1,35 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const passport = require('passport');
 
 const app = express();
 const port = process.env.PORT || 5600;
 
+// Load User Model
+require('./models/User');
+
+// Passport Config
+require('./config/passport')(passport);
+
+// Load Routes
+const auth = require('./routes/auth');
+
+// Load Keys
+const keys = require('./config/keys');
+
+// Map Global Promises
+mongoose.Promise = global.Promise;
+
+// mongoose Connect
+mongoose.connect(keys.mongoURI)
+    .then(() => console.log('MongoDB Connected'))
+    .catch(err => console.log(err));
 app.get('/', (req, res) => {
     res.send('Hello');
 });
+
+// Use Routes
+app.use('/auth', auth);
 
 app.listen(port, () => {
     console.log(`Server started on port ${port}`)
